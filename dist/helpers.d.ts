838fc7b98c74b598c3f3f7ff2f363dcc7994ea7c
Aaron Splady <asplady@pfl.com>
2021-10-19 10:40:22 -0600

    removed dist folder

--- dist/helpers.d.ts
+++
@@ -1,8 +0,0 @@
-import { Container } from "@azure/cosmos";
-export declare const isCosmosDbContainer: (maybeContainer: any) => maybeContainer is Container;
-export declare type Logger = {
-    debug(message?: string): void;
-    info(message?: string): void;
-    warn(message?: string): void;
-    error(message?: string): void;
-};
